9ba3b532dc2b44486f1344a31f734a5272dba2ad
annaali1 <44680525+annaali1@users.noreply.github.com>
2018-12-06 01:57:16 -0800

    Add files via upload
    added function to change QLabel when user is either admin or user

---
+++ login.h
@@ -0,0 +1,27 @@
+#ifndef LOGIN_H
+#define LOGIN_H
+
+#include <QDialog>
+
+namespace Ui {
+class logIn;
+}
+
+class logIn : public QDialog
+{
+    Q_OBJECT
+
+public:
+    explicit logIn(QWidget *parent = 0);
+    void CurrentUserRights();
+    ~logIn();
+    
+private slots:
+    void on_pushButton_clicked();
+
+private:
+    Ui::logIn *ui;
+    bool access;
+};
+
+#endif // LOGIN_H
